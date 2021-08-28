3e73a6bb67fe1055a399ec18cd3ed9228370e0b8
Aihab Umair <addisonhardley111@gmail.com>
2021-08-28 09:19:44 +0530

    Created frontend layout

---
+++ ohlc-engine/src/components/dashboard/dashboard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Drawer from '../drawer/drawer'
+import Navbar from '../navbar/navbar'
+import * as classes from './dashboard.module.css'
+
+const dashboard = () => {
+    return (
+        <div className={classes.outerDiv}>
+            <Drawer />
+            <div className={classes.dashboard}>
+                <Navbar />
+                <div className={classes.innerDash}>
+                    <div className={classes.innerDiv}>
+                        <div className={classes.infoButtons}>
+                            <h1>Good Morning.</h1>
+                            <p>Here's what's going on with stocks today.</p>
+                            <div className={classes.filter}>
+                                <input placeholder="Search"></input>
+                                <div className={classes.monthFilter}>
+                                    Filter by date:
+                                    <input type="month"></input>
+                                    to
+                                    <input type="month"></input>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.chart}>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div className={classes.chartChanger}>
+                            <div>Chart Type 1</div>
+                            <div>Chart Type 2</div>
+                            <div>Chart Type 3</div>
+                            <div>Chart Type 4</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default dashboard;
