8e3727c7141629d96933d3c4710723c8082d9038
Rishabh Jain <jain.rishabhche@gmail.com>
2021-08-28 15:10:40 +0530

    Added charting feature

--- ohlc-engine/src/components/chart/Chart.js
+++ ohlc-engine/src/components/chart/Chart.js
@@ -1,225 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { format } from "d3-format";
-import { timeFormat } from "d3-time-format";
+import { scaleTime } from "d3-scale";
+import { utcDay } from "d3-time";
 
 import { ChartCanvas, Chart } from "react-stockcharts";
-import {
-  BarSeries,
-  OHLCSeries,
-  LineSeries,
-  MACDSeries,
-} from "react-stockcharts/lib/series";
+import { CandlestickSeries } from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
-import {
-  CrossHairCursor,
-  EdgeIndicator,
-  MouseCoordinateX,
-  MouseCoordinateY,
-} from "react-stockcharts/lib/coordinates";
-
-import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
-import {
-  OHLCTooltip,
-  MovingAverageTooltip,
-  MACDTooltip,
-} from "react-stockcharts/lib/tooltip";
-import {
-  ema,
-  macd,
-  change,
-  elderImpulse,
-} from "react-stockcharts/lib/indicator";
 import { fitWidth } from "react-stockcharts/lib/helper";
-import { last } from "react-stockcharts/lib/utils";
+import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
 
-const macdAppearance = {
-  stroke: {
-    macd: "#FF0000",
-    signal: "#00F300",
-  },
-  fill: {
-    divergence: "#4682B4",
-  },
-};
-
-class OHLCChartWithElderImpulseIndicator extends React.Component {
+class CandleStickChart extends React.Component {
   render() {
-    const changeCalculator = change();
-
-    const ema12 = ema()
-      .id(1)
-      .options({ windowSize: 12 })
-      .merge((d, c) => {
-        d.ema12 = c;
-      })
-      .accessor((d) => d.ema12);
-
-    const macdCalculator = macd()
-      .options({
-        fast: 12,
-        slow: 26,
-        signal: 9,
-      })
-      .merge((d, c) => {
-        d.macd = c;
-      })
-      .accessor((d) => d.macd);
-
-    const elderImpulseCalculator = elderImpulse()
-      .macdSource(macdCalculator.accessor())
-      .emaSource(ema12.accessor());
-
-    const { type, data: initialData, width, ratio } = this.props;
-
-    const calculatedData = elderImpulseCalculator(
-      macdCalculator(ema12(changeCalculator(initialData)))
-    );
-    const xScaleProvider = discontinuousTimeScaleProvider.inputDateAccessor(
-      (d) => d.date
-    );
-    const { data, xScale, xAccessor, displayXAccessor } =
-      xScaleProvider(calculatedData);
-
-    const start = xAccessor(last(data));
-    const end = xAccessor(data[Math.max(0, data.length - 150)]);
-    const xExtents = [start, end];
-
+    const { type, width, data, ratio } = this.props;
+    console.log(this.props);
+    const xAccessor = (d) => new Date(d.date);
+    const xExtents = [
+      xAccessor(last(data)),
+      xAccessor(data[data.length - 100]),
+    ];
     return (
       <ChartCanvas
-        height={500}
-        width={width}
+        height={400}
         ratio={ratio}
-        margin={{ left: 70, right: 70, top: 20, bottom: 30 }}
+        width={width}
+        margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
         type={type}
         seriesName="MSFT"
         data={data}
-        xScale={xScale}
         xAccessor={xAccessor}
-        displayXAccessor={displayXAccessor}
+        xScale={scaleTime()}
         xExtents={xExtents}
       >
-        <Chart
-          id={1}
-          height={300}
-          yExtents={(d) => [d.high, d.low]}
-          padding={{ top: 10, bottom: 10 }}
-        >
-          <XAxis
-            axisAt="bottom"
-            orient="bottom"
-            showTicks={false}
-            outerTickSize={0}
-          />
-          <YAxis axisAt="right" orient="right" ticks={2} />
-
-          <MouseCoordinateY
-            at="right"
-            orient="right"
-            displayFormat={format(".2f")}
-          />
-
-          <LineSeries yAccessor={ema12.accessor()} stroke={ema12.stroke()} />
-
-          <OHLCSeries
-            stroke={(d) => elderImpulseCalculator.stroke()[d.elderImpulse]}
-          />
-
-          <EdgeIndicator
-            itemType="last"
-            orient="right"
-            edgeAt="right"
-            yAccessor={(d) => d.close}
-            fill={(d) => (d.close > d.open ? "#6BA583" : "#FF0000")}
-          />
-
-          <OHLCTooltip origin={[-40, -10]} />
-          <MovingAverageTooltip
-            onClick={(e) => console.log(e)}
-            origin={[-38, 5]}
-            options={[
-              {
-                yAccessor: ema12.accessor(),
-                type: "EMA",
-                stroke: ema12.stroke(),
-                windowSize: ema12.options().windowSize,
-              },
-            ]}
-          />
+        <Chart id={1} yExtents={(d) => [d.high, d.low]}>
+          <XAxis axisAt="bottom" orient="bottom" ticks={6} />
+          <YAxis axisAt="left" orient="left" ticks={5} />
+          <CandlestickSeries width={timeIntervalBarWidth(utcDay)} />
         </Chart>
-        <Chart
-          id={2}
-          height={150}
-          yExtents={(d) => d.volume}
-          origin={(w, h) => [0, h - 300]}
-        >
-          <YAxis
-            axisAt="left"
-            orient="left"
-            ticks={5}
-            tickFormat={format(".2s")}
-          />
-
-          <MouseCoordinateY
-            at="left"
-            orient="left"
-            displayFormat={format(".4s")}
-          />
-
-          <BarSeries
-            yAccessor={(d) => d.volume}
-            fill={(d) => (d.close > d.open ? "#6BA583" : "#FF0000")}
-            opacity={0.4}
-          />
-        </Chart>
-        <Chart
-          id={3}
-          height={150}
-          yExtents={macdCalculator.accessor()}
-          origin={(w, h) => [0, h - 150]}
-          padding={{ top: 10, bottom: 10 }}
-        >
-          <XAxis axisAt="bottom" orient="bottom" />
-          <YAxis axisAt="right" orient="right" ticks={2} />
-
-          <MouseCoordinateX
-            at="bottom"
-            orient="bottom"
-            displayFormat={timeFormat("%Y-%m-%d")}
-          />
-          <MouseCoordinateY
-            at="right"
-            orient="right"
-            displayFormat={format(".2f")}
-          />
-
-          <MACDSeries yAccessor={(d) => d.macd} {...macdAppearance} />
-          <MACDTooltip
-            origin={[-38, 15]}
-            yAccessor={(d) => d.macd}
-            options={macdCalculator.options()}
-            appearance={macdAppearance}
-          />
-        </Chart>
-        <CrossHairCursor />
       </ChartCanvas>
     );
   }
 }
 
-OHLCChartWithElderImpulseIndicator.propTypes = {
+CandleStickChart.propTypes = {
   data: PropTypes.array.isRequired,
   width: PropTypes.number.isRequired,
   ratio: PropTypes.number.isRequired,
   type: PropTypes.oneOf(["svg", "hybrid"]).isRequired,
 };
 
-OHLCChartWithElderImpulseIndicator.defaultProps = {
+CandleStickChart.defaultProps = {
   type: "svg",
 };
-OHLCChartWithElderImpulseIndicator = fitWidth(
-  OHLCChartWithElderImpulseIndicator
-);
+CandleStickChart = fitWidth(CandleStickChart);
 
-export default OHLCChartWithElderImpulseIndicator;
+export default CandleStickChart;
--- ohlc-engine/src/library/modules/reader/reader.js
+++ ohlc-engine/src/library/modules/reader/reader.js
@@ -3,12 +3,12 @@ const stockData = require("../../../srv/stocks.json");
 let hashedData = {};
 
 stockData.forEach((stock) => {
-    hashedData[stock.symbol] = [];
-})
+  hashedData[stock.symbol] = [];
+});
 
 stockData.forEach((stock) => {
-    hashedData[stock.symbol] = [...hashedData[stock.symbol], (stock)];
-})
+  hashedData[stock.symbol] = [...hashedData[stock.symbol], stock];
+});
 
 module.exports = {
   hashedData: hashedData,
